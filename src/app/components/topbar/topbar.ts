cc55bc4c2bf97754c6368861823271d29743d97c
Valentin Duffet <vavadu07@gmail.com>
2026-01-07 22:44:03 +0100

    feat: add topbar

--- src/app/components/topbar/topbar.ts
+++ src/app/components/topbar/topbar.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './topbar.html',
   styleUrl: './topbar.scss',
 })
 export class Topbar {
+  private router = inject(Router);
 
+  protected isOpenToWork = signal<boolean>(false);
+  protected urlToDisplay = signal<string>('');
+
+  constructor() {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((router) => {
+        this.urlToDisplay.set(router.url);
+      });
+  }
 }
